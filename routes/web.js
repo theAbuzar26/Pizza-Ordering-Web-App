20253ab896a4aec86256f69e2b60f8fe67d2cd96
Abuzar <starkeqbal@gmail.com>
2023-08-13 12:53:41 +0530

    Auth system added

--- routes/web.js
+++ routes/web.js
@@ -3,6 +3,9 @@ const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
 
+// Middlewares 
+const guest = require('../app/http/middlewares/guest')
+
 function initRoutes(app){
 
     app.get('/', homeController().index)
@@ -11,10 +14,13 @@ function initRoutes(app){
     //     res.render('home');
     // }) 
      
-    app.get('/login', authController().login)
- 
-    app.get('/register', authController().register)
-   
+    app.get('/login', guest,authController().login);
+    app.post('/login', authController().postLogin)
+    app.get('/register', guest,authController().register);
+    app.post('/register', authController().postRegister);
+    app.post('/logout', authController().logout)
+
+
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
 
